311c381d592f28f94f55cf910a99b392f85e87a2
Elias Souza <elias.ssouza@hotmail.com>
2021-04-28 23:09:32 -0300

    atualização

--- lib/main.dart
+++ lib/main.dart
@@ -65,9 +65,19 @@ class MyHomePage extends StatelessWidget {
                             color: Colors.purple,
                           ))),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(tr.title),
-                      Text(tr.date.toString()),
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ],
                   )
                 ],
